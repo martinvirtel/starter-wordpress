9589d28a7a877a5cda614bb37fd41fb8145d643c
Martin Virtel <martin.virtel@gmail.com>
2018-11-16 17:09:58 +0000

    clean up older zips, make sure only run once

--- upload-service.sh
+++ upload-service.sh
@@ -23,17 +23,34 @@ export LOG_ERROR_FILE_PATH="/tmp/${myname}.error.log"
 . services.sh
 
 
+TIME_FORMAT='%F %H:%M'
+OUTPUT_FORMAT='%T Event(s): %e fired for file: %w. Refreshing.'
+RUNNING_FILE=/tmp/$0.$$.running
+
 run-service() {
   local action="$1" # Action
 
-  while inotifywait -q -r -e close_write \
-                      --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" "$SOURCEDIR"; do
-    echo "@@@ Running action '${action}'"
-    export LATEST=$SOURCEDIR/$(ls --sort=time $SOURCEDIR | head -1)
-    cd $STATICDIR
-    unzip -u -o $LATEST 
-    env AWS_DEFAULT_REGION=eu-central-1 AWS_PROFILE=newslab-prototype-webspace-writer aws s3 sync --no-progress --acl=public-read  ./ s3://dpa-newslab-prototype-webspace/dpa-stories/beta/
-    date "+%Y-%m-%dT%H:%M:%S ---- Uploaded $LATEST" 
+  while inotifywait -q -e close_write \
+                    --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" "$SOURCEDIR"; do
+    if [ ! -f $RUNNING_FILE ] ; then
+	    touch $RUNNING_FILE 
+	    export LATEST=$SOURCEDIR/$(ls --sort=time $SOURCEDIR | head -1)
+	    cd $STATICDIR
+	    echo "Unpacking $LATEST"
+	    unzip -u -o $LATEST  2>&1 >/dev/null
+	    # index file should not be there
+	    printf "Updated $(date)\n"  > index.html
+	    # no feed
+	    rm -rf feed/* 
+	    env AWS_DEFAULT_REGION=eu-central-1 AWS_PROFILE=newslab-prototype-webspace-writer aws s3 sync --acl=public-read --quiet ./ s3://dpa-newslab-prototype-webspace/dpa-stories/beta/
+	    date "+%Y-%m-%dT%H:%M:%S ---- Uploaded $LATEST" 
+	    # find $SOURCEDIR -mtime +10
+	    # find html/wp-content/plugins/simply-static/static-files | sort -r 
+	    echo Keeping the last 40 versions, removing the rest
+	    rm -v $(find $SOURCEDIR | sort -r | sed 1,40d)
+	else 
+	    echo $RUNNING_FILE already present - skipping
+	fi
   done
 }
 
@@ -41,14 +58,14 @@ before-start() {
   local action="$1" # Action
   mkdir -p $STATICDIR
   cd $STATICDIR
-  echo "* Starting with $action"
+  # echo "* Starting with $action"
 }
 
 after-finish() {
   local action="$1" # Action
   local serviceExitCode=$2 # Service exit code
 
-  echo "* Finish with $action. Exit code: $serviceExitCode"
+  # echo "* Finish with $action. Exit code: $serviceExitCode"
 }
 
 action="$1"
