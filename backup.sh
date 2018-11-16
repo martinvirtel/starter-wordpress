8e045c990ebf16738028b9ddc151ff0dc5a9e36d
Martin Virtel <martin.virtel@gmail.com>
2018-11-16 13:28:32 +0000

    with path

--- backup.sh
+++ backup.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
+set -Eeuo pipefail
+
+if [ "${DEBUG:-}" != "" ] ; then
+    set -x
+fi
+
 
 export PATH=/home/ubuntu/bin:/home/ubuntu/.local/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
+if [ -L $0 ] ; then
+	cd $(dirname $(readlink $0))
+fi
 make run-backup >backup.log 2>&1
 
