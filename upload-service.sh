963e6c432443a2190df1f0c4777ac7199afd5df4
Martin Virtel <martin.virtel@gmail.com>
2019-01-26 16:14:49 +0000

    wait for silence before upload

--- upload-service.sh
+++ upload-service.sh
@@ -9,7 +9,7 @@ export STATICDIR=/home/ubuntu/projekte/stories-wordpress-7/html/tmp
 
 export PID_FILE_PATH="/tmp/${myname}.pid"
 export LOG_FILE_PATH="/tmp/${myname}.log"
-export LOG_ERROR_FILE_PATH="/tmp/${myname}.error.log"
+export LOG_ERROR_FILE_PATH="/tmp/${myname}.log"
 
 export SCRIPTVERSION=$(ls -l $0)
 #
@@ -29,19 +29,39 @@ export SCRIPTVERSION=$(ls -l $0)
 TIME_FORMAT='%F %H:%M:%S'
 OUTPUT_FORMAT='%T Event(s): %e fired for file: %w. Refreshing.'
 RUNNING_FILE=/tmp/$0.$$.running
+COUNTERFILE=/tmp/$$-$(basename $0)-counter
 
 process_files() {
    echo making files writable
    sudo chgrp -R ubuntu * 
+   sudo chgrp ubuntu . 
    sudo chmod -R g+w *
+   sudo chmod -R g+w .
    echo overwriting index file 
    printf "Updated $(date)\n"  > index.html
    echo removing feeds
    rm -rf feed/* 
+   rm -rf comments/* 
    echo rewriting absolute links
    sed -i.original 's_"/wp-content/_"/dpa-newslab-prototype-webspace/dpa-stories/beta/wp-content/_g' $(find . -type f | egrep '(js|html|css)$')
 
 }
+
+
+wait_for_silence() {
+
+while /bin/true
+  do
+        inotifywatch -e close -t 4 -r $SOURCEDIR  2>&1 | \
+        tee $COUNTERFILE
+  grep -c 'No events' $COUNTERFILE && return
+  echo "Next 5 secs"
+
+  done
+
+}
+
+
 
 run-service() {
   local action="$1" # Action
@@ -51,25 +71,25 @@ run-service() {
                     --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" "$SOURCEDIR"; do
     if [ ! -f $RUNNING_FILE ] ; then
 	    touch $RUNNING_FILE 
-            echo Waiting 40 seconds for export to finish
-	    sleep 40
+            echo Waiting for 5 seconds without close events from $SOURCEDIR
+	    wait_for_silence
 	    # export LATEST=$SOURCEDIR/$(ls --sort=time $SOURCEDIR | head -1)
 	    cd $STATICDIR
 	    # echo "Unpacking $LATEST"
 	    # unzip -u -o $LATEST  2>&1 >/dev/null
 	    process_files
-	    AWS_DEFAULT_REGION=eu-central-1 AWS_PROFILE=newslab-prototype-webspace-writer aws s3 sync --acl=public-read ./ s3://dpa-newslab-prototype-webspace/dpa-stories/beta/
+	    AWS_DEFAULT_REGION=eu-central-1 AWS_PROFILE=newslab-prototype-webspace-writer aws s3 sync --acl=public-read --quiet ./ s3://dpa-newslab-prototype-webspace/dpa-stories/beta/
 	    date "+%Y-%m-%dT%H:%M:%S ---- Upload of $STATICDIR ready" 
 	    # find $SOURCEDIR -mtime +10
 	    # find html/wp-content/plugins/simply-static/static-files | sort -r 
 	    # echo Keeping the last 40 versions, removing the rest
 	    # rm -v $(find $SOURCEDIR -type f | sort -r | sed 1,40d)
-	    trap { rm -f $RUNNING_FILE } exit 
 	    rm $RUNNING_FILE
 	else 
 	    echo $RUNNING_FILE already present - skipping
 	fi
   done
+  trap "{ rm -f $RUNNING_FILE $COUNTERFILE; echo removing temp files }" exit 
 }
 
 before-start() {
