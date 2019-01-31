e21054c256cc1aeb3ba2a8e1b6802665551e50b7
Martin Virtel <martin.virtel@gmail.com>
2019-01-31 23:56:32 +0000

    config!

--- upload-service.sh
+++ upload-service.sh
@@ -1,11 +1,16 @@
 #! /bin/bash
 
-export myname=$(basename $0)
+set -x
 
-# export SOURCEDIR=$(pwd)/html/wp-content/plugins/simply-static/static-files
-export SOURCEDIR=/home/ubuntu/projekte/stories-wordpress-7/html/tmp
-export LATEST=$SOURCEDIR/$(ls --sort=time $SOURCEDIR | head -1)
-export STATICDIR=/home/ubuntu/projekte/stories-wordpress-7/html/tmp
+# Configuration contains BUCKETPREFIX
+. upload-service.conf
+
+export myname=$(basename $(pwd))-$(basename $0)
+export SOURCEDIR=$(dirname $0)/html/tmp
+export BUCKETNAME=dpa-newslab-prototype-webspace
+export BUCKETURL=s3://$BUCKETNAME$BUCKETPREFIX
+export DESTPREFIX=/$BUCKETNAME$BUCKETPREFIX
+
 
 export PID_FILE_PATH="/tmp/${myname}.pid"
 export LOG_FILE_PATH="/tmp/${myname}.log"
@@ -28,10 +33,11 @@ export SCRIPTVERSION=$(ls -l $0)
 
 TIME_FORMAT='%F %H:%M:%S'
 OUTPUT_FORMAT='%T Event(s): %e fired for file: %w. Refreshing.'
-RUNNING_FILE=/tmp/$0.$$.running
-COUNTERFILE=/tmp/$$-$(basename $0)-counter
+RUNNING_FILE=/tmp/$myname.$$.running
+COUNTERFILE=/tmp/$myname.counter
 
 process_files() {
+   echo processing in $(pwd)
    echo making files writable
    sudo chown -R www-data:ubuntu * 
    sudo chown -R www-data:ubuntu . 
@@ -43,7 +49,8 @@ process_files() {
    rm -rf feed/* 
    rm -rf comments/* 
    echo rewriting absolute links
-   sed -i.original 's_"/wp-content/_"/dpa-newslab-prototype-webspace/dpa-stories/beta/wp-content/_g' $(find . -type f | egrep '(js|html|css)$')
+   sed -i.original 's_"/wp-content/_"'$DESTPREFIX'wp-content/_g;'"s_'/wp-content/_'${DESTPREFIX}wp-content/_g"\
+		     $(find . -type f | egrep '(js|html|css)$')
 
 }
 
@@ -52,51 +59,46 @@ wait_for_silence() {
 
 while /bin/true
   do
-        inotifywatch -e close -t 4 -r $SOURCEDIR  2>&1 | \
+        inotifywatch -e close -t 4 -r ./  2>&1 | \
         tee $COUNTERFILE
   grep -c 'No events' $COUNTERFILE && return
-  echo "Next 5 secs"
+  echo "$(pwd) - watching for silence, next 5 secs"
 
   done
 
 }
 
+upload_files() {
 
+ AWS_DEFAULT_REGION=eu-central-1 AWS_PROFILE=newslab-prototype-webspace-writer aws s3 sync --acl=public-read --quiet ./ $BUCKETURL 
+
+}
 
 run-service() {
   local action="$1" # Action
-
-  echo watching $SOURCEDIR
+  cd $SOURCEDIR
+  echo watching $(pwd)
   while inotifywait -q -e close_write \
-                    --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" "$SOURCEDIR"; do
+                    --timefmt "${TIME_FORMAT}" --format "${OUTPUT_FORMAT}" "./"; do
     if [ ! -f $RUNNING_FILE ] ; then
 	    touch $RUNNING_FILE 
-            echo Waiting for 5 seconds without close events from $SOURCEDIR
 	    wait_for_silence
-	    # export LATEST=$SOURCEDIR/$(ls --sort=time $SOURCEDIR | head -1)
-	    cd $STATICDIR
-	    # echo "Unpacking $LATEST"
-	    # unzip -u -o $LATEST  2>&1 >/dev/null
 	    process_files
-	    AWS_DEFAULT_REGION=eu-central-1 AWS_PROFILE=newslab-prototype-webspace-writer aws s3 sync --acl=public-read --quiet ./ s3://dpa-newslab-prototype-webspace/dpa-stories/beta/
-	    date "+%Y-%m-%dT%H:%M:%S ---- Upload of $STATICDIR ready" 
-	    # find $SOURCEDIR -mtime +10
-	    # find html/wp-content/plugins/simply-static/static-files | sort -r 
-	    # echo Keeping the last 40 versions, removing the rest
-	    # rm -v $(find $SOURCEDIR -type f | sort -r | sed 1,40d)
+            upload_files
+	    date "+%Y-%m-%dT%H:%M:%S ---- Upload of $SOURCEDIR ready" 
 	    rm $RUNNING_FILE
 	else 
 	    echo $RUNNING_FILE already present - skipping
 	fi
   done
-  trap "{ rm -f $RUNNING_FILE $COUNTERFILE; echo removing temp files }" exit 
+  trap "{ rm -f $RUNNING_FILE $COUNTERFILE; echo removing temp files; }" exit 
 }
 
 before-start() {
   local action="$1" # Action
-  mkdir -p $STATICDIR
-  cd $STATICDIR
-  echo "Starting $SCRIPTVERSION with $action"
+  mkdir -p $SOURCEDIR
+  cd $SOURCEDIR
+  echo "Starting $SCRIPTVERSION with $action in $SOURCEDIR"
 }
 
 after-finish() {
