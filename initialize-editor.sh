d3d192a0c135b95f2bac0b3f56be2ef5fbb8c739
Martin Virtel <martin.virtel@gmail.com>
2019-02-02 08:54:06 +0000

    basic plugins

---
+++ initialize-editor.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+# -o pipefail fail immediately if error in pipe
+# -e fail immediately on error
+# -E subshells inherit ERR handler
+# -u Error on unset variables
+# -x Echo all lines to STDERR
+# -k get assignments 
+
+set -Eeuo pipefail
+
+if [ "${DEBUG:-}" != "" ] ; then
+    set -x
+fi
+
+
+
+for A in \
+	advanced-gutenberg \
+	duplicate-post \
+	genie-wp-favicon \
+	ghostkit \
+	gutenberg \
+	show-modified-date-in-admin-lists \
+	simply-static \
+	smart-slider-3 \
+	under-construction-page \
+; do 
+
+	make cli CLI="plugin install --activate $A"
+
+done
