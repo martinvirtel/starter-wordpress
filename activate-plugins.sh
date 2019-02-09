23beec7c7a25ca2d59c8703b01d92e1d7dac2331
Martin Virtel <martin.virtel@gmail.com>
2019-02-09 08:32:03 +0000

    helpers

---
+++ activate-plugins.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+# -o pipefail fail immediately if error in pipe
+# -e fail immediately on error
+# -E subshells inherit ERR handler
+# -u Error on unset variables
+# -x Echo all lines to STDERR
+# -k get assignments 
+
+set -Eeuo pipefail
+
+if [ "${DEBUG:-}" != "" ] ; then
+    set -x
+fi
+
+
+
+for A in $(make cli CLI="plugin list" | awk '/inactive/ { print $1 }')
+  do 
+
+	make cli CLI="plugin activate $A"
+
+done
+
+
+
